840a49a94a9c8ebc9a0920bb97115e5b49ad4465
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2014-04-23 19:27:09 +0200

    Create Pair.hpp

---
+++ src/Pair.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file src/Pair.hpp
+ *
+ * @date 29-mar-2014
+ *
+ * @author Youri Hoogstrate
+ *
+ * @section LICENSE
+ * segmentation-fold can predict RNA 2D structures including K-turns.
+ * Copyright (C) 2012-2014 Youri Hoogstrate
+ *
+ * This file is part of segmentation-fold.
+ *
+ * segmentation-fold is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * segmentation-fold is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#define Pair std::pair<unsigned int, unsigned int>
