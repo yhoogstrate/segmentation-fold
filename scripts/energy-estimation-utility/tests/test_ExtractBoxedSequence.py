7515ef5465946142e1a5225598c246190962c4c4
yhoogstrate <y.hoogstrate@gmail.com>
2016-04-26 12:34:49 +0200

    More utils

--- scripts/energy-estimation-utility/tests/test_ExtractBoxedSequence.py
+++ scripts/energy-estimation-utility/tests/test_ExtractBoxedSequence.py
@@ -24,26 +24,21 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import unittest,logging,sys,filecmp
 logging.basicConfig(level=logging.DEBUG,format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",stream=sys.stdout)
 
-from segmentation_fold_utils.FindBoxes import FindBoxes
+from segmentation_fold_utils.ExtractBoxedSequences import ExtractBoxedSequences
 
-class TestFindBoxes(unittest.TestCase):
-	def test_01(self):
-		# Chech whether pattern inversion works as expected
-		boxes = FindBoxes('/dev/null','NRUGAUG','CUGA',True,True)
-		
-		self.assertEqual( boxes.box1r , 'CATCAYN' )# R = A||G -> rc(A||G) = T||C = Y
-		self.assertEqual( boxes.box2r , "TCAG" )# CUGA -> CTGA -> (rev) AGUC -> (rc) TCAG
-	
-	def test_02(self):
-		input_file = 'FindBoxes.genome.fa'
-		output_file = "FindBoxes.test_02.bed"
-		boxes = FindBoxes('tests/test-data/'+input_file,'NRUGAUG','CUGA',True,True)
-		boxes.run(open(output_file,"w"))
-		
-		self.assertTrue(filecmp.cmp(output_file,"tests/test-data/"+output_file) )
+class TestExtractBoxedSequences(unittest.TestCase):
+    def test_01(self):
+        input_fasta_file = 'FindBoxes.genome.fa'
+        input_bed_file = "FindBoxes.test_02.bed"
+        output_fasta_file = "ExtractBoxedSequences.test_01.fa"
+        sequences = ExtractBoxedSequences('tests/test-data/'+input_fasta_file,input_bed_file,open('tests/test-data/'+output_fasta_file,'r'),90,10)
+        sequences.run(open(output_fasta_file,"w"))
+        
+        #self.assertTrue(filecmp.cmp(output_file,"tests/test-data/"+output_file) )
 
 def main():
-	unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-	main()
+    main()
+
