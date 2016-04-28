3cc9606822ea9fa49975e5c4bbbd51054d432063
yhoogstrate <y.hoogstrate@gmail.com>
2016-04-28 13:41:56 +0200

    Initial steps for CLI/estimate-energy

--- scripts/energy-estimation-utility/segmentation_fold_utils/CLI.py
+++ scripts/energy-estimation-utility/segmentation_fold_utils/CLI.py
@@ -33,50 +33,36 @@ def CLI():
 	pass
 
 
-@CLI.command(name='energy-estimation')
-def CLI_energy_estimation():
-	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,epilog="For more info please visit:\n<https://github.com/yhoogstrate/segmentation-fold>")
+def get_sequence_and_segments(xml_file,fasta_file):
+	xml = parse_xml(xml_file)
 	
-	parser.add_argument("-t","--temp-dir",default="/tmp",help="Directory in which the temporary files will be created")
-	parser.add_argument("-s","--segmentation-fold",default="segmentation-fold",help="Path of the binary")
-	parser.add_argument("-x","--xml-file",default="/usr/local/share/segmentation-fold/segments.xml",help="Location of segments.xml")
-	
-	parser.add_argument("-r","--randomize",default=0,type=int,help="Randomize or shuffle the sequence -r times; default 0 means this function is disabled")
-	
-	return parser.parse_args()
+	if sequence_from_xml_file:
+		for sequence, segment in xml.pairs():
+			yield sequence,segment
+	else:
+		# Assert len input_fasta_file == 1
+		for sequence in parse_fasta(input_fasta_file[0]):
+			for segment in xml.segments():
+				yield sequence,segment
 
 
-@CLI.command(name='scan-for-segments')
-def CLI_scan_for_segments():
-	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,epilog="For more info please visit:\n<https://github.com/yhoogstrate/segmentation-fold>")
-	
-	parser.add_argument("-t","--temp-dir",default="/tmp",help="Directory in which the temporary files will be created")
-	parser.add_argument("-T","--threads",default=2,type=int,help="Used threads per spawned instance of segmentation-fold")
-	parser.add_argument("-s","--segmentation-fold",default="segmentation-fold",help="Path of the binary")
-	parser.add_argument("-x","--xml-file",default="/usr/local/share/segmentation-fold/segments.xml",help="Location of segments.xml")
-	parser.add_argument("-p","--precision",choices=['quick','in-depth'],default="in-depth",help="Precision level 'quick' will scan quickly whether the structure changes and 'in-depth' will try to find the transition levels.")
-	
-	parser.add_argument("fasta_file",help="FASTA file containing the sequences that will be scanned for the presence of segments from --xml-file")
-	
-	return parser.parse_args()
+@CLI.command(name="estimate-energy",short_help="Estimates whether a certain Segment(Loop) is present and for which delta-G this transistion takes place")
+@click.option("--temp-dir","-t",type=click.Path(exists=True),default="/tmp",help="Ppath in which temporary conifig files will be stored (default: /tmp)")
+@click.option("--segmentation-fold","-s",default="segmentation-fold",help="Location of segmentatio-fold binary (default: segmentation-fold)")
+@click.option("--xml-file","-x", type=click.File('r'),default="/usr/local/share/segmentation-fold/segments.xml",help="Location of segments.xml (default: /usr/local/share/segmentation-fold/segments.xml)")
+@click.option("--threads","-T",default=2,type=int,help="Number of threads per spawned instance of segmentation-fold")
+@click.option("--precision","-p",default=0.0025,help="Minimal difference for binary split - the smaller this value the slower. if this value equals 0, the difference is set to infinity (default: 0.0025)")
+@click.option("--randomize","-r",default=0,type=int,help="Randomize or shuffle each sequence this many times; (default: 0, 0 means disabled)")
+@click.option("--sequences_from_fasta_file","-f", type=click.File('r'),default=None,help="Use sequences from a FASTA file instead of using the XML file that also contains the segments. In XML files you can explicitly link one Segment(Loop) to one particular sequence instead of doing n*n comparisons (default: None)")
+@click.argument('output_dbn_file', type=click.File('w'))
+def CLI_energy_estimation(temp_dir,segmentation_fold,xml_file,threads,precision,randomize,sequences_from_fasta_file,output_dbn_file):
+	print sequences_from_fasta_file
+	#for sequence, segment in get_sequence_and_segments(xml_file,fasta_file):
+	#	do_test(sequence, segment)
 
 
-@CLI.command(name='cd-box',short_help='Scans through a sequence for subsequences that may contain C/D-box K-turns')
-@click.argument('input_fasta_file', type=click.Path(exists=True))
-@click.argument('output_bed_file', type=click.File('w'))
-@click.option('--box1',default='NRUGAUG',help="The first motif/box (default is C-box: NRUGAUG)")
-@click.option('--box2',default='CUGA',help="The second motif/box (default is D-box: CUGA)")
-@click.option('--forward/--no-foward',help="Scan in the forward strand of the genome (default: True / --forward)")
-@click.option('--reverse/--no-foward',help="Scan in the reverse complement strand of the genome (default: True / --reverse)")
-@click.option('--inner-dist','-d',type=int, default=250,help="The maximal distance between the boxes (default=250).")
-def CLI_scan_for_cd_box_kturns(input_fasta_file,box1,box2,forward,reverse,output_bed_file):
-	boxes = FindBoxes(input_fasta_file,box1,box2,forward,reverse,inner_dist)
-	boxes.run(output_bed_file)
-	#sequences = ExtractBoxedSequences(fasta_input_file,bed_input_file,fasta_output_file,max_inner_dist,bp_extension)
-	#sequences.run(fasta_output_file)
 
-
-@CLI.command(name='find-boxes',short_help='find all occurances of two given boxes (sequence motifs) within a FASTA file')
+@CLI.command(name='find-boxes',short_help='Finds all occurances of two given boxes (sequence motifs) within a FASTA file')
 @click.argument('fasta_input_file',  type=click.Path(exists=True))
 @click.argument('bed_output_file', type=click.File('w'))
 @click.option('--box1','-c',default='NRUGAUG',help="Sequence of box1 (default = C-box: 'NRUGAUG')")
@@ -88,7 +74,7 @@ def CLI_extract_boxed_sequences(fasta_input_file,bed_output_file,box1,box2,forwa
     boxes.run(bed_output_file)
 
 
-@CLI.command(name='extract-boxed-sequences',short_help='bed_input_file has to be  created with \'find-box\' as part of this utility')
+@CLI.command(name='extract-boxed-sequences',short_help='Extracts boxes sequences from bed_input_file which has to be created with \'find-box\', part of this utility')
 @click.argument('fasta_input_file',  type=click.Path(exists=True))
 @click.argument('bed_input_file', type=click.File('r'))
 @click.argument('fasta_output_file', type=click.File('w'))
